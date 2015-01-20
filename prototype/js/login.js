fcc5cef05c0c0c7bc655402be541baf0fdb34630
Michaelcj10 <michaelcjames10@gmail.com>
2015-01-20 12:57:29 +0000

    cookie set

--- prototype/js/login.js
+++ prototype/js/login.js
@@ -135,6 +135,8 @@ $(document).ready(function() {
             	if($_user_password_edit.val() == data.Password)
            {
 
+       checkCookie();
+       
              window.location="html/home.html";
          }
          else { alert("Wrong password");}
@@ -189,5 +191,50 @@ $(document).ready(function() {
 			});
 		
 	});
+
+
+
+
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname+"="+cvalue+"; "+expires;
+}
+
+function getCookie(cname) {
+
+
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("username");
+  
+   
+    if (user != "") {
+        alert("Welcome again " + user);
+
+
+    } else {
+       user = $_user_username_field.val();
+      
+       if (user != "" && user != null) {
+           setCookie("username", user, 5);
+       }
+    }
+}
+
+
 });
 
