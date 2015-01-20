aad2be10900aa03b7afa1051fb91f5a6a6985443
Michaelcj10 <michaelcjames10@gmail.com>
2015-01-20 18:24:28 +0000

    session storage

--- prototype/js/home.js
+++ prototype/js/home.js
@@ -6,56 +6,11 @@
 
 
 
-
-
-
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname+"="+cvalue+"; "+expires;
-}
-
-function getCookie(cname) {
-
-
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-
-        }
-    }
-    return "";
-}
-
-function checkCookie(user_cookie) {
-    var user=getCookie("username");
-    var name = user_cookie;
-   
-    if (user == name) {
-        alert("Home page cookie " + user);
-         
-
-    } else {
-       user = $_user_username_field.val();
-      
-       if (user != "" && user != null) {
-           setCookie("username", user, 5);
-       }
-    }
-}
-
-
 function setStorage() {
 
 
 
-var user=getCookie("username");
-localStorage.setItem("username", user);
+
     
    document.getElementById("demo").innerHTML = localStorage.getItem("username");
 }
--- prototype/js/login.js
+++ prototype/js/login.js
@@ -134,10 +134,10 @@ $(document).ready(function() {
 
             	if($_user_password_edit.val() == data.Password)
            {
-
-       checkCookie();
+           	
        
              window.location="html/home.html";
+             setStorage();
          }
          else { alert("Wrong password");}
 
@@ -195,44 +195,13 @@ $(document).ready(function() {
 
 
 
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname+"="+cvalue+"; "+expires;
-}
-
-function getCookie(cname) {
+function setStorage() {
 
 
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
 
-        }
-    }
-    return "";
-}
-
-function checkCookie() {
-    var user=getCookie("username");
+var user= $("#get_user_username_input").val();
+localStorage.setItem("username", user);
   
-   
-    if (user ==$_user_username_field.val()) {
-        alert("Welcome again " + user);
-
-
-    } else {
-       user = $_user_username_field.val();
-      
-       if (user != "" && user != null) {
-           setCookie("username", user, 5);
-       }
-    }
 }
 
 
