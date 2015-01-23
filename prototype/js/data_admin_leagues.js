eb260dbb613f7e9241a50862e345553bd671744c
Martin Zuber <zubidlo@gmail.com>
2015-01-23 23:22:26 +0000

    data_admin_leagues done

--- prototype/js/data_admin_leagues.js
+++ prototype/js/data_admin_leagues.js
@@ -10,43 +10,39 @@ var dataAdminLeaguesCode = function() {
 	
 	var $_get_by_id_form = $("#get_by_id_form");
 	var $_id_field = $("#get_id_input");
-	var $_username_field = $("#get_username_input");
-	var $_id_edit = $("#id_edit");
-	var $_username_edit = $("#username_edit");
-	var $_password_edit = $("#password_edit");
-	var $_email_edit = $("#email_edit");
-	var $_get_by_username_form = $("#get_by_username_form");
 	var $_edit_form = $("#edit_form");
+	var $_id_edit = $("#id_edit");
+	var $_name_edit = $("#name_edit");
+	var $_nextfixtures_edit = $("#nextfixtures_edit");
+	var $_week_edit = $("#week_edit");
 
-	var readUserFromInputFields = function () {
+	var readLeagueFromInputFields = function () {
 		
 		return {
 			Id : $_id_edit.val(),
-			Username : $_username_edit.val(),
-			Password : $_password_edit.val(),
-			Email : $_email_edit.val()
+			Name : $_name_edit.val(),
+			NextFixtures : $_nextfixtures_edit.val(),
+			Week : $_week_edit.val()
 		};
 	}
 
-	var fillUserTextFields = function (user) {
+	var fillLeagueTextFields = function (league) {
 
-		$_id_edit.val(user.Id);
-		$_username_edit.val(user.Username);
-		$_password_edit.val(user.Password);
-		$_email_edit.val(user.Email);
+		$_id_edit.val(league.Id);
+		$_name_edit.val(league.Name);
+		$_nextfixtures_edit.val(league.NextFixtures);
+		$_week_edit.val(league.Week);
 	}
 
 	//examples:
-	//.../api/users?$orderby=Username --> get all user ordered by username
-	//.../api/users?$filter=Email eq 'zubidlo'gmail.com' --> get all users which Email equals 'zubidlo'gmail.com'
-	//.../api/users?$top=10&$skip=30 --> get 10 users but skip first 30 (31,32.....,40)
+	//.../api/leagues?$orderby=Name --> get all leagues ordered by username
 	//injects table of top items into DOM
 	//page.top : how many rows the table will have
 	//page.skip: how many rows to skip
 	//example: page.top=10 and page.skip=0 --> table with first 10 items
 	//page.top=10 and page.skip=10 --> table with from 11 to 20 items
 	//page.top=10 and page.skip=20 --> table with from 21 to 30 items
-	var getUsers = function(page) {
+	var getLeagues = function(page) {
 
 		var url = _url + "?$top=" + page.top + "&$skip=" + page.skip;
 		var successCallback = function (data, textStatus, request) {
@@ -54,15 +50,15 @@ var dataAdminLeaguesCode = function() {
         	var counter_start = page.skip;
 			var headers = [
 				"Id<span>(PK)</span>",
-				"Username<span>(R)</span>",
-				"Password<span>(R)</span>",
-				"Email<span>(R)</span>"
+				"Name<span>(R)</span>",
+				"NextFixtures<span>(R)</span>",
+				"Week<span>(R)</span>"
 			];
 			var properties = [
-			"Id",
-				"Username",
-				"Password",
-				"Email"
+				"Id",
+				"Name",
+				"NextFixtures",
+				"Week"
 			];
 			buildTable(counter_start, headers, properties, data);
     	}
@@ -75,7 +71,7 @@ var dataAdminLeaguesCode = function() {
 
 		_count = parseInt(data.length);
 		$_table_rows_count.val(_count);
-		getUsers(tableCurrentPage());
+		getLeagues(tableCurrentPage());
 	});
 	
 	$_table_rows_form.submit(function(event) {
@@ -83,19 +79,19 @@ var dataAdminLeaguesCode = function() {
 		event.preventDefault();
 		_top = $_table_rows_input.val();
 		_skip = 0;
-		getUsers(tableCurrentPage());
+		getLeagues(tableCurrentPage());
 	});
 
 	$_previous_page_form.submit(function(event) {
 
 		event.preventDefault();
-		getUsers(tablePreviousPage());
+		getLeagues(tablePreviousPage());
 	});
 
 	$_next_page_form.submit(function(event) {
 
 		event.preventDefault();
-		getUsers(tableNextPage());
+		getLeagues(tableNextPage());
 	});
 
 	//user by id GET request
@@ -105,22 +101,9 @@ var dataAdminLeaguesCode = function() {
 		var url = _url + "/id/" + $_id_field.val();
 		var successCallback = function(data, textStatus, request) {
 			
-			fillUserTextFields(data);
+			fillLeagueTextFields(data);
             printOutput(textStatus, request);
 		}
-		ajaxRequest(url, successCallback, generalErrorCallback);
-	});
-
-	//user by username GET request
-	$_get_by_username_form.submit(function(event) {
-
-		event.preventDefault();
-		var url = _url + "/username/" + $_username_field.val();
-		var successCallback = function(data, textStatus, request) {
-            	
-        	fillUserTextFields(data);
-        	printOutput(textStatus, request);
-    	}
 		ajaxRequest(url, successCallback, generalErrorCallback);
 	});
 
@@ -130,28 +113,28 @@ var dataAdminLeaguesCode = function() {
 		event.preventDefault();
 
 		var url = _url;
-		var user = readUserFromInputFields();
+		var league = readLeagueFromInputFields();
 		var successCallback = function (data, textStatus, request) {
             
         	ajaxRequest(_url, function(data, textStatus, request) {
 
 				_count = parseInt(data.length);
 				$_table_rows_count.val(_count);
-				getUsers(tableCurrentPage());
+				getLeagues(tableCurrentPage());
 			});
         	printOutput(textStatus, request);
     	}
 		var type = $("option:checked").val();
 		if (type === "PUT") {
-			url = url + "/id/" + user.Id;
+			url = url + "/id/" + league.Id;
 		}
 		else if (type === "DELETE") {
-			url = url + "/id/" + user.Id;
-			user = "undefined";
+			url = url + "/id/" + league.Id;
+			league = "undefined";
 		}
 		var dataType = "json";
 
-		ajaxRequest(url, successCallback, generalErrorCallback, type, dataType, user);
+		ajaxRequest(url, successCallback, generalErrorCallback, type, dataType, league);
 	});
 }
 
